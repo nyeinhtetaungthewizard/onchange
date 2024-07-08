7defeb545b4f23d66011fcab748a71931dc0f67e
Ko Nyein Htet Aung ( Steven ) <147907222+nyeinhtetaungthewizard@users.noreply.github.com>
2024-07-08 12:38:56 +0630

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+// import Button from './Button.jsx'
+import MyComponent from './MyComponent.jsx';
+// import Picture from './Picture.jsx';
+// import Counter from './Counter.jsx'
+function App(){
+    return (
+        <>
+           <MyComponent />  
+        </>
+    );
+}
+export default App;
